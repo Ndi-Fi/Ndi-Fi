05bd70f18394edfa0b2897416601c9906d6d9d1f
Edoka Isaac <edokaizkdaking247@gmail.com>
2025-07-30 22:47:14 +0100

    Pulled latest changes from main branch

--- src/Staking.sol
+++ src/Staking.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
+// Function stake 
+// Function withdraw
+// Function claim
+// Function getBalance
+// Struct StakeInfo {amount, timestamp, bool claimed{}
+
+Stakes[] public listOfStakes;
+contract Staking {
+    
+}
