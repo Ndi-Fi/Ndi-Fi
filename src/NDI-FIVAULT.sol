e1c6aa2b8d073c437e2fe6178a4630f2d43390c5
Bright Bassey <basseyu01@gmail.com>
2025-08-08 16:02:46 +0100

    ADD: ADMIN FUNCTIONS AND VIEW FUNCTIONS

--- src/NDI-FIVAULT.sol
+++ src/NDI-FIVAULT.sol
@@ -43,7 +43,11 @@ contract NDIFIVault is ERC4626, Ownable {
         _;
     }
 
-    constructor(address DaiTokenAddress, string memory name, string memory symbol)
+    constructor(
+        address DaiTokenAddress,
+        string memory name,
+        string memory symbol
+    )
         ERC4626(IERC20(DaiTokenAddress))
         ERC20(name, symbol)
         Ownable(initialOwner)
@@ -57,7 +61,10 @@ contract NDIFIVault is ERC4626, Ownable {
     }
 
     // core functions
-    function deposit(uint256 amount, address receiver)
+    function deposit(
+        uint256 amount,
+        address receiver
+    )
         public
         override
         onlyWhenNotPaused
@@ -75,12 +82,11 @@ contract NDIFIVault is ERC4626, Ownable {
         return super.deposit(amount, receiver);
     }
 
-    function withdraw(uint256 amount, address receiver, address _owner)
-        public
-        override
-        onlyWhenNotPaused
-        returns (uint256)
-    {
+    function withdraw(
+        uint256 amount,
+        address receiver,
+        address _owner
+    ) public override onlyWhenNotPaused returns (uint256) {
         if (receiver == address(0) || _owner == address(0)) {
             revert InvalidAddress();
         }
@@ -91,7 +97,10 @@ contract NDIFIVault is ERC4626, Ownable {
         return super.withdraw(amount, receiver, _owner);
     }
 
-    function mint(uint256 shares, address receiver) public override onlyWhenNotPaused returns (uint256) {
+    function mint(
+        uint256 shares,
+        address receiver
+    ) public override onlyWhenNotPaused returns (uint256) {
         if (receiver == address(0)) revert InvalidAddress();
         if (shares <= 0) revert invalidDepositAmount();
         if (shares > DAI.balanceOf(msg.sender)) revert invalidDepositAmount();
@@ -103,12 +112,11 @@ contract NDIFIVault is ERC4626, Ownable {
         return super.mint(shares, receiver);
     }
 
-    function redeem(uint256 shares, address receiver, address _owner)
-        public
-        override
-        onlyWhenNotPaused
-        returns (uint256)
-    {
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address _owner
+    ) public override onlyWhenNotPaused returns (uint256) {
         if (receiver == address(0)) revert InvalidAddress();
         if (shares <= 0) revert invalidAmount();
 
@@ -122,19 +130,27 @@ contract NDIFIVault is ERC4626, Ownable {
         return super.totalAssets();
     }
 
-    function previewWithdraw(uint256 DaiAsset) public view override returns (uint256) {
+    function previewWithdraw(
+        uint256 DaiAsset
+    ) public view override returns (uint256) {
         return super.previewWithdraw(DaiAsset);
     }
 
-    function previewDeposit(uint256 DaiAsset) public view override returns (uint256) {
+    function previewDeposit(
+        uint256 DaiAsset
+    ) public view override returns (uint256) {
         return super.previewDeposit(DaiAsset);
     }
 
-    function previewMint(uint256 shares) public view override returns (uint256) {
+    function previewMint(
+        uint256 shares
+    ) public view override returns (uint256) {
         return super.previewMint(shares);
     }
 
-    function previewRedeem(uint256 shares) public view override returns (uint256) {
+    function previewRedeem(
+        uint256 shares
+    ) public view override returns (uint256) {
         return super.previewRedeem(shares);
     }
 
@@ -150,7 +166,7 @@ contract NDIFIVault is ERC4626, Ownable {
     function emergencyWithdraw() public onlyOwner {
         uint256 balance = DAI.balanceOf(address(this));
         if (balance > 0) {
-            DAI.safeTransfer(owner, balance);
+            DAI.safeTransfer(initialOwner, balance);
             emit withdrawSuccessful();
         }
     }
@@ -158,7 +174,7 @@ contract NDIFIVault is ERC4626, Ownable {
     function emergencyRedeem() public onlyOwner {
         uint256 shares = super.balanceOf(address(this));
         if (shares > 0) {
-            super.redeem(shares, owner, address(this));
+            super.redeem(shares, initialOwner, address(this));
             emit redeemed();
         }
     }
--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {TokenStaking} from "../src/Staking.sol";
+
+// import {TokenStaking} from "../src/Staking.sol";
 
 //Test
 contract stakingTest is Test {
