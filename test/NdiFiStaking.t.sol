a183a9c91cc86bd818cd6d88ff5e1f4aedc3f5a1
TheCreatorNode <basseyu01@gmail.com>
2025-08-20 21:59:27 +0100

    ADD: staking contract logic

--- test/NdiFiStaking.t.sol
+++ test/NdiFiStaking.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.30;
 
 import {Test, console} from "forge-std/Test.sol";
-import {NdiFiStaking} from "../src/NdiFiStaking.sol";
+import {NdiStaking} from "../src/NdiFiStaking.sol";
 import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 // Simple mintable ERC20
